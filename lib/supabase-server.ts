19da1cc5750b1b843e2db5c58dd6c603a0d71638
Marcel Spahr <xrp@bluewin.ch>
2026-02-02 12:50:48 +0100

    Fix missing createSupabaseServerClient export

--- lib/supabase-server.ts
+++ lib/supabase-server.ts
@@ -1,8 +1,24 @@
 import { createClient } from '@supabase/supabase-js';
+import { cookies } from 'next/headers';
 
-export const getSupabaseBrowserClient = () => {
+export function createSupabaseServerClient() {
+  const cookieStore = cookies();
+
   return createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      auth: {
+        persistSession: false,
+      },
+      global: {
+        headers: {
+          Cookie: cookieStore
+            .getAll()
+            .map((c) => `${c.name}=${c.value}`)
+            .join('; '),
+        },
+      },
+    }
   );
-};
+}
